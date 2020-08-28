2b3dcc14efe34e98d6f7bdfe3ec3043ed3f912cb
Geraint White <geraint.white@bt.com>
2020-08-28 15:05:25 +0100

    Add pod-install test

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Geraint White on 28/08/2020.
+//  Copyright © 2020 Geraint White. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
